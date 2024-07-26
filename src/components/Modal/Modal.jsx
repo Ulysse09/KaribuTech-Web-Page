b908436276edb449840188edae519df3e50ee67a
Ulysse09 <upkabalisa@gmail.com>
2024-07-26 15:22:57 +0200

    23rd com

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Modal = ({ handletoggle }) => {
   return (
     <>
-      <div className="absolute font-roboto lg:hidden z-50 items-start pl-4 pt-4 pb-4  inset-5 top-[4rem] bottom-[10rem]      flex flex-col bg-slate-100 bg-gradient-to-b from-white   text-blue-400 space-y-6">
+      <div className="absolute font-roboto lg:hidden z-50 items-start pl-4 pt-4 pb-4  inset-5 top-[4rem] bottom-[8rem]      flex flex-col bg-slate-100 bg-gradient-to-b from-white   text-blue-400 space-y-6">
         <Link
           to={"/"}
           className="hover:border-b-2 border-b-purple-500 p-2 transition-all duration-100"
@@ -21,7 +21,7 @@ const Modal = ({ handletoggle }) => {
         </Link>
 
         <Link
-          to="service"
+          to="services"
           className="hover:border-b-2 border-b-purple-500 p-2 transition-all duration-100"
           onClick={handletoggle}
         >
